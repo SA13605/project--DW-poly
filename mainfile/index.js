e77cbd0f3ce91a0fc904b97b76efadf695ccee86
SA13605 <samyakpopulwad@gmail.com>
2025-08-05 22:26:32 +0530

    fix the problem

--- mainfile/index.js
+++ mainfile/index.js
@@ -1,11 +1,31 @@
 let menuBar = document.querySelector(".menuBar");
 let menu = document.querySelector(".menu");
+let showWishList = document.querySelector(".showWishList");
+let showProfile = document.querySelector(".showProfile");
 
 menuBar.addEventListener('click', function() {
     console.log("Menu bar clicked");
     menu.style.display = (menu.style.display === "block") ? "none" : "block";
+
+    showWishList.style.display = (showWishList.style.display === "block") ? "none" : "none";
+    showProfile.style.display = (showProfile.style.display === "block") ? "none" : "none";
 });
 
+document.querySelector(".wishList").addEventListener("click",()=>{
+    showWishList.style.display = (showWishList.style.display === "block") ? "none" : "block";
+
+    showProfile.style.display = (showProfile.style.display === "block") ? "none" : "none";
+    menu.style.display = (menu.style.display === "block") ? "none" : "none";
+});
+
+document.querySelector(".profile").addEventListener("click",()=>{
+    showProfile.style.display = (showProfile.style.display === "block") ? "none" : "block";
+
+    menu.style.display = (menu.style.display === "block") ? "none" : "none";
+    showWishList.style.display = (showWishList.style.display === "block") ? "none" : "none";
+});
+
+// hiro imgs
 let hiroImg = document.querySelector(".hiroImg");
 
 document.querySelector(".hiro1").addEventListener('click', function() {
